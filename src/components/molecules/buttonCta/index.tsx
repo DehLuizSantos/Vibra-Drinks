69c9b9de6b7896e081640ea14bea78dd5caec0df
André Luiz Santos <and_bas7@hotmail.com>
2026-04-09 15:02:26 -0300

    feat/filter-menu-card-opinion

--- src/components/molecules/buttonCta/index.tsx
+++ src/components/molecules/buttonCta/index.tsx
@@ -4,9 +4,9 @@ export default function ButtonCta() {
   return (
     <Link
       href={"/orcamento"}
-      className="border border-gold bg-pink-300 p-6 text-white-500 rounded-2xl w-full flex text-center drop-shadow-[0_5px_5px_rgba(255,176,1,0.4)]"
+      className="border border-gold bg-pink-300 p-6 text-white-500 rounded-2xl w-full flex text-center drop-shadow-[0_5px_5px_rgba(255,176,1,0.4)] md:h-20"
     >
-      <p className="mx-auto display-font">FAÇA UM ORÇAMENTO JÁ</p>
+      <p className="mx-auto display-font md:text-2xl">FAÇA UM ORÇAMENTO JÁ</p>
     </Link>
   );
 }
--- src/components/molecules/menuOpiniao/index.tsx
+++ src/components/molecules/menuOpiniao/index.tsx
@@ -1,25 +1,32 @@
+"use client";
+import { useAppMenuStore } from "@/store/useMenuStore";
+import { useStore } from "zustand";
+
+type MenuOpiniaoProp = {
+  value: "casamento" | "corporativos" | "intimos";
+  label: string;
+};
+
 export default function MenuOpiniao() {
-  const menuOptions = [
-    {
-      value: "casamentos",
-      label: "CASAMENTOS"
-    },
-    {
-      value: "corporativos",
-      label: "EVENTOS CORPORATIVOS"
-    },
-    {
-      value: "intimos",
-      label: "EVENTOS INTIMOS"
-    }
+  const menuOptions: MenuOpiniaoProp[] = [
+    { value: "casamento", label: "CASAMENTOS" },
+    { value: "corporativos", label: "EVENTOS CORPORATIVOS" },
+    { value: "intimos", label: "EVENTOS INTIMOS" }
   ];
+
+  const { menuType, setMenuType } = useStore(useAppMenuStore);
 
   return (
     <div className="flex items-center justify-around my-3.5">
       {menuOptions.map(menu =>
         <div
-          className="rounded-full border border-gold w-23 h-23 md:w-36.5 md:h-36.5 flex items-center justify-center"
+          className={`rounded-full 
+            ${menuType === menu.value ? "border-3" : "border"}
+            ${menuType === menu.value &&
+              "drop-shadow-[0_5px_5px_rgba(255,176,1,0.4)]"}
+             border-gold w-23 h-23 md:w-36.5 md:h-36.5 flex items-center justify-center   `}
           key={menu.value}
+          onClick={() => setMenuType(menu.value)}
         >
           <p className="text-xs display-marcellus text-gold text-center  p-1">
             {menu.label}
